65bbf42cb2e60e735b4800724c0f6595a791bd24
William Kao <bill.kao2010@gmail.com>
2022-03-23 23:12:30 -0400

    Rock Paper Scissors

---
+++ RockPaperScissors/app.js
@@ -0,0 +1,5 @@
+const ComputerChoice = document.getElementById("computer-choice")
+const UserChoice = document.getElementById("user-choice")
+const resultDisplay = document.getElementById("result")
+const possibleChoice = document.querySelectorAll('button')
+
